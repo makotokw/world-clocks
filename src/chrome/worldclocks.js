308cafd07754b13de8eed7b9000dc4f178f06e72
makoto_kw <makoto.kw@gmail.com>
2012-04-28 17:48:39 +0900

    Removed common files

---
+++ src/chrome/worldclocks.js
@@ -0,0 +1,34 @@
+var WorldClocks = {};
+(function(w) {
+    function empty() {};
+    function msg(key, args) {
+        if (typeof(chrome) != "undefined" && typeof(chrome.i18n) != "undefined") {
+            return chrome.i18n.getMessage(key, args);
+        }
+        return key;
+    }
+    function setPref(key, value) {
+        try {
+            if (window.localStorage) {
+                localStorage[key] = value;
+            }
+        } catch (e) {
+        }
+    }
+    function getPref(key, defaultValue) {
+        var value = defaultValue;
+        try {
+            if (window.localStorage) {
+                value = localStorage[key] || defaultValue;
+            }
+        } catch (e) {
+        }
+        return value;
+    }
+    
+    w.msg = msg;
+    w.pref = {get:getPref, set:setPref};
+    w.log = (typeof(console) != "undefined" && typeof(console.log) != "undefined") ? console.log : empty;
+    
+})(WorldClocks);
+
