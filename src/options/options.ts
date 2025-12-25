8caa39f7c53142a1326e376b0f39467dbdf462a8
makoto_kw <makoto.kw@gmail.com>
2025-12-25 18:45:40 +0900

    Remove Bootstrap in options

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,55 +1,73 @@
-import $ from 'jquery';
-window.jQuery = window.$ = $;
-import 'bootstrap/dist/css/bootstrap.css';
-await import('bootstrap/dist/js/bootstrap');
 import '@/common/scripts/coolclock';
 import '@/common/scripts/coolclock_patch';
 import '@/common/scripts/coolclock_moreskins';
 import { WorldClocks } from '@/common/scripts/worldclocks';
 
-(function ($) {
+(function () {
   const w: any = WorldClocks;
 
   // localize
-  const messages = {
+  const messages: Record<string, string> = {
     'option_group_icon': 'ICON_GROUP',
     'option_group_popup': 'POPUP_GROUP',
     'skin_label': 'SKIN_LABEL',
     'show_footer_label_text': 'SHOW_FOOTER_LABEL',
     'show_footer_help': 'SHOW_FOOTER_HELP',
   };
-  $.each(messages, function (key, value) {
-    $('#' + key).html(w.msg(value));
-  });
-  $('#title').html(w.msg('OPTION_TITLE'));
+
+  for (const [key, value] of Object.entries(messages)) {
+    const el = document.getElementById(key);
+    if (el) {
+      el.innerHTML = w.msg(value);
+    }
+  }
+
+  const titleEl = document.getElementById('title');
+  if (titleEl) {
+    titleEl.innerHTML = w.msg('OPTION_TITLE');
+  }
 
   const version = chrome.runtime.getManifest().version;
-  $('#copyright').html(w.msg('APP_TITLE') + ' ' + version);
+  const copyrightEl = document.getElementById('copyright');
+  if (copyrightEl) {
+    copyrightEl.innerHTML = w.msg('APP_TITLE') + ' ' + version;
+  }
 
   // skin
-  let skin = w.pref.get('ba_skin'),
-    $skin_select = $('#skin_select');
-  $.each((CoolClock as any).config.skins, function (attr: any) {
-    $skin_select.append('<option value="' + attr + '">' + attr + '</option>');
-  });
-  $skin_select.change(function () {
-    skin = $(this).val();
-    try {
-      chrome.runtime.sendMessage({
-        type: 'setSkin',
-        target: 'offscreen',
-        skinId: skin
-      }).then(() => {});
-    } catch (e) {
+  let skin = w.pref.get('ba_skin');
+  const skinSelect = document.getElementById('skin_select') as HTMLSelectElement;
+
+  if (skinSelect) {
+    for (const attr in (window as any).CoolClock.config.skins) {
+      const option = document.createElement('option');
+      option.value = attr;
+      option.textContent = attr;
+      skinSelect.appendChild(option);
     }
-    w.pref.set('ba_skin', skin);
-  }).val(skin);
+
+    skinSelect.value = skin;
+    skinSelect.addEventListener('change', () => {
+      skin = skinSelect.value;
+      try {
+        chrome.runtime.sendMessage({
+          type: 'setSkin',
+          target: 'offscreen',
+          skinId: skin
+        }).then(() => { });
+      } catch (e) {
+      }
+      w.pref.set('ba_skin', skin);
+    });
+  }
 
   // footer
-  const showFooter = ('false' != w.pref.get('showFooter', 'true'));
-  $('#show_footer').change(function () {
-    const show = $(this).is(':checked');
-    w.pref.set('showFooter', show);
-  }).attr({ checked: showFooter });
-
-})(jQuery);
+  const showFooter = ('false' !== w.pref.get('showFooter', 'true'));
+  const showFooterCheckbox = document.getElementById('show_footer') as HTMLInputElement;
+  if (showFooterCheckbox) {
+    showFooterCheckbox.checked = showFooter;
+    showFooterCheckbox.addEventListener('change', () => {
+      const show = showFooterCheckbox.checked;
+      w.pref.set('showFooter', show);
+    });
+  }
+})();
