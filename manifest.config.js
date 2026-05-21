38f59a811bf0be82d19c75bd35727983b3c15859
makoto_kw <makoto.kw@gmail.com>
2026-05-22 00:29:41 +0900

    Update manifest icons paths, remove unused resources, and add new icon sizes

--- manifest.config.js
+++ manifest.config.js
@@ -1,13 +1,19 @@
 import { defineManifest } from '@crxjs/vite-plugin';
 import pkg from './package.json';
 
+// https://developer.chrome.com/docs/extensions/reference/manifest
 export default defineManifest({
   name: '__MSG_APP_TITLE__',
   description: '__MSG_APP_DESCRIPTION__',
   version: pkg.version,
   manifest_version: 3,
   default_locale: 'en',
-  permissions: ['alarms', 'offscreen'],
+  icons: {
+    16: 'icon_16.png',
+    32: 'icon_32.png',
+    48: 'icon_48.png',
+    128: 'icon_128.png',
+  },
   background: {
     service_worker: 'src/background/service-worker.ts',
     type: 'module',
@@ -18,19 +24,12 @@ export default defineManifest({
   },
   action: {
     default_icon: {
-      16: 'public/icon_16.png',
-      128: 'public/icon_128.png',
+      16: 'icon_16.png',
+      32: 'icon_32.png',
+      48: 'icon_48.png',
+      128: 'icon_128.png',
     },
     default_popup: 'src/popup/popup.html',
   },
-  icons: {
-    16: 'public/icon_16.png',
-    128: 'public/icon_128.png',
-  },
-  web_accessible_resources: [
-    {
-      resources: ['src/background/background.html'],
-      matches: ['<all_urls>'],
-    },
-  ],
+  permissions: ['alarms', 'offscreen'],
 });
